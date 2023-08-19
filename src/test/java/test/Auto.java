9ae4119fd86dd4ae83a6e9fa922f7eb1e745c625
dsilvau <125933418+dsilvau@users.noreply.github.com>
2023-08-19 10:28:26 -0500

    8

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -21,9 +21,25 @@ public class Auto {
 	}
 	
 	public String verificarIntegridad() {
-		
-	}
-
+		int x = 0;
+		int y = 0;
+		if(this.registro == motor.registro) {
+			for(int i= 0; i < asientos.length; i++) {
+				if (asientos[i].getClass().getName() == "Asiento") {
+					x ++;
+					if(asientos[i].registro == motor.registro) {
+					y++;	
+					}
+				}			
+		    } 	
+	    }
+		if (x == y) {
+			return "Auto original";
+		}
+		else {
+			return "Las piezas no son originales";
+		}
+    }
 	
 
 }
